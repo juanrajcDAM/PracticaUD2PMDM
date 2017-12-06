9fd1a8e12ca0f18d69dffce55e3ae6e05c8d4aab
juanrajc <juanrajc@hotmail.com>
2017-12-06 17:20:01 +0100

    Commit de partes no subidas.

---
+++ app/src/main/java/com/htcdiurno/practicaud2pmdm/DatePickerFragment.java
@@ -0,0 +1,38 @@
+package com.htcdiurno.practicaud2pmdm;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+import java.util.Calendar;
+
+/**
+ * @author Juan Ramón Jover Cruz
+ */
+public class DatePickerFragment extends DialogFragment {
+
+    private DatePickerDialog.OnDateSetListener listener;
+
+    public static DatePickerFragment newInstance(DatePickerDialog.OnDateSetListener listener) {
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setListener(listener);
+        return fragment;
+    }
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //Usa la fecha actual como fecha por defecto al mostrarse.
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Crea una nueva instancia de DatePickerDialog y la devuelve.
+        return new DatePickerDialog(getActivity(), listener, year, month, day);
+    }
+}
---
+++ app/src/main/java/com/htcdiurno/practicaud2pmdm/Registro.java
@@ -0,0 +1,138 @@
+package com.htcdiurno.practicaud2pmdm;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author Juan Ramón Jover Cruz
+ */
+public class Registro extends AppCompatActivity {
+
+    private EditText nombreRegistro, emailRegistro, fNacRegistro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+    }
+
+    /**
+     *Método que muestra un DatePicker, en el que seleccionaremos una fecha y la escribirá
+     * en el EditText de la fecha de nacimiento.
+     *
+     * @param view
+     */
+    public void mostrarCalendario(View view) {
+
+        //Enlace al recurso de la interfaz en el que se escribe la fecha de nacimento.
+        fNacRegistro =findViewById(R.id.fNacRegistro);
+
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because january is zero
+                final String selectedDate = day + " / " + (month + 1) + " / " + year;
+                fNacRegistro.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(getFragmentManager(), "datePicker");
+
+    }
+
+    /**
+     *Método que valida los datos introducidos.
+     *
+     * @param view
+     */
+    public void validar(View view){
+
+        //Enlace a los recursos de la interfaz.
+        nombreRegistro = findViewById(R.id.nombreRegistro);
+        emailRegistro = findViewById(R.id.emailRegistro);
+        fNacRegistro = findViewById(R.id.fNacRegistro);
+
+        //Si algún campo de texto está vacío, muestra un mensaje...
+        if(nombreRegistro.getText().length()<1 || emailRegistro.getText().length()<1 || fNacRegistro.getText().length()<1) {
+            Toast.makeText(getApplicationContext(), "Falta algún campo por rellenar.", Toast.LENGTH_SHORT).show();
+        //Si no...
+        }else{
+            //Comprueba si es mayor de edad. Si no lo es, muestra un mensaje...
+            if(!calculaEdad(fNacRegistro.getText().toString()))
+                Toast.makeText(getApplicationContext(), "No puedes registrarte, no eres mayod de edad.", Toast.LENGTH_SHORT).show();
+            //Si es mayor de edad, muestra un mensaje.
+            else
+                Toast.makeText(getApplicationContext(), "Usuario registrado.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /**
+     * Método que finaliza la view.
+     *
+     * @param view
+     */
+    public void volver(View view){
+
+        finish();
+
+    }
+
+    /**
+     * Método que comprueba si la fecha de nacimiento corresponde a una persona mayor o menor de edad.
+     *
+     * @param fecha Parámetro con la fecha de nacimento en formato cadena de caracteres.
+     * @return True o false, dependiendo de si es o no mayor de edad.
+     */
+    public boolean calculaEdad(String fecha){
+
+        Date fechaNac=null;
+
+        try {
+            //Pasa el String con la fecha a formato Date.
+            fechaNac = new SimpleDateFormat("dd / MM / yyyy").parse(fecha);
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), "Error con la fecha.", Toast.LENGTH_SHORT).show();
+        }
+
+        Calendar fechaNacimiento = Calendar.getInstance();
+
+        //Se crea un objeto con la fecha actual.
+        Calendar fechaActual = Calendar.getInstance();
+
+        //Se asigna la fecha recibida a la fecha de nacimiento.
+        fechaNacimiento.setTime(fechaNac);
+
+        //Se restan la fecha actual y la fecha de nacimiento.
+        int anio = fechaActual.get(Calendar.YEAR)- fechaNacimiento.get(Calendar.YEAR);
+        int mes =fechaActual.get(Calendar.MONTH)- fechaNacimiento.get(Calendar.MONTH);
+        int dia = fechaActual.get(Calendar.DATE)- fechaNacimiento.get(Calendar.DATE);
+
+        //Se ajusta el año dependiendo el mes y el día.
+        if(mes<0 || (mes==0 && dia<0)){
+            anio--;
+        }
+
+        //Si la persona tiene menos de 18 años...
+        if(anio<18)
+            //Devuelve false.
+            return false;
+        //Si tiene mas de 18 años...
+        else
+            //Devuelve true.
+            return true;
+
+    }
+
+}
